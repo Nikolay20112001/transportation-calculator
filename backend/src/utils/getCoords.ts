612a45fd18dea657088f9705f992a51daa18b5b1
Dmitrii Sakulin <elaydweb@gmail.com>
2025-01-14 17:00:56 +0300

    add cache for cords and distance

--- backend/src/utils/getCoords.ts
+++ backend/src/utils/getCoords.ts
@@ -2,18 +2,25 @@ import axios from 'axios';
 import { ErrorsDescriptions } from '../enums/errorsDescriptions';
 import { AppError } from '../helpers/errorHandler';
 import HttpStatusCode from '../enums/httpStatusCodes';
+import { cacheManager } from '../helpers/redisCache';
 
 export async function getCoords(address: string) {
-  const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
-  try {
-    const response = await axios.get(url);
-    if (response.data && response.data.length > 0) {
-      const { lat, lon } = response.data[0];
-      return { lat: parseFloat(lat), lon: parseFloat(lon) };
-    } else {
-      throw new Error('Адрес не найден');
+  const cacheKey = `coords:${address}`;
+
+  const coords = await cacheManager.cacheRequest(cacheKey, async () => {
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
+    try {
+      const response = await axios.get(url);
+      if (response.data && response.data.length > 0) {
+        const { lat, lon } = response.data[0];
+        return { lat: parseFloat(lat), lon: parseFloat(lon) };
+      } else {
+        throw new Error('Адрес не найден');
+      }
+    } catch (error) {
+      throw new AppError(ErrorsDescriptions.GET_COORDS_ERROR, true, error, HttpStatusCode.BAD_REQUEST);
     }
-  } catch (error) {
-    throw new AppError(ErrorsDescriptions.GET_COORDS_ERROR, true, error, HttpStatusCode.BAD_REQUEST);
-  }
+  });
+
+  return coords;
 }
